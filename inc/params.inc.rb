6f74c54d730130362aea460223b788bf789fab87
ScientificProgressGoesBoing <enahel@web.de>
2014-05-04 02:29:52 +0200

    add inc directory to tracked files

---
+++ inc/params.inc.rb
@@ -0,0 +1,5 @@
+MAB_TXT = './mab/mab4DS.txt'
+BEG = '###'											#parameter: sign that indicates begin of set of data	
+NUM = 4												#parameter: length of field name
+DICT_TXT = './inc/dictionary.inc.rb'				#parameter: dictionary file name
+DICT_SEP = '::'						#parameter: separator in dictionary file
